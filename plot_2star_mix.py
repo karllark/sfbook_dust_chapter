0488f0a9c2ba026514d1465b3e18e20a5085e765
Karl Gordon <kgordon@stsci.edu>
2023-02-16 13:38:55 -0500

    updating and running black

--- plot_2star_mix.py
+++ plot_2star_mix.py
@@ -4,67 +4,70 @@ import matplotlib as mpl
 
 import astropy.units as u
 
-from dust_extinction.dust_extinction import G16
+from dust_extinction.parameter_averages import G16
 
 # setup the plots
 fontsize = 16
-font = {'size'   : fontsize}
+font = {"size": fontsize}
 
-mpl.rc('font', **font)
+mpl.rc("font", **font)
 
-mpl.rc('lines', linewidth=2)
-mpl.rc('axes', linewidth=2)
-mpl.rc('xtick.major', width=2)
-mpl.rc('ytick.major', width=2)
-mpl.rc('xtick.minor', width=2)
-mpl.rc('ytick.minor', width=2)
+mpl.rc("lines", linewidth=2)
+mpl.rc("axes", linewidth=2)
+mpl.rc("xtick.major", width=2)
+mpl.rc("ytick.major", width=2)
+mpl.rc("xtick.minor", width=2)
+mpl.rc("ytick.minor", width=2)
 
 fig, ax = plt.subplots()
 
-x = np.arange(0.3,10.0,0.1)/u.micron
+x = np.arange(0.3, 10.0, 0.1) / u.micron
 
 # input extinction curve
 ext_model = G16()
-ax.plot(1./x,ext_model(x),'k--', label='input extinction')
+ax.plot(1.0 / x, ext_model(x), "k--", label="input extinction")
 
 # stellar intrinsic fluxes
-f1 = np.full(len(x),1.0)
-f2 = np.full(len(x),1.0)
+f1 = np.full(len(x), 1.0)
+f2 = np.full(len(x), 1.0)
 
 f12 = f1 + f2
 
-av1 = [1.0,0.5,0.2,0.0]
-av2 = [2.0,2.0,2.0,2.0]
+av1 = [1.0, 0.5, 0.2, 0.0]
+av2 = [2.0, 2.0, 2.0, 2.0]
 
 # get the index for the "V" band
-sindxs = np.argsort(np.abs(x - (1.0/0.55)/u.micron))
+sindxs = np.argsort(np.abs(x - (1.0 / 0.55) / u.micron))
 
 for k in range(len(av1)):
-    
+
     # attenuated
-    rf1 = f1*ext_model.extinguish(x, Av=av1[k])
-    rf2 = f2*ext_model.extinguish(x, Av=av2[k])
+    rf1 = f1 * ext_model.extinguish(x, Av=av1[k])
+    rf2 = f2 * ext_model.extinguish(x, Av=av2[k])
 
     rf12 = rf1 + rf2
 
     # attenuation curve
-    att12 = -2.5*np.log10(rf12/f12)
+    att12 = -2.5 * np.log10(rf12 / f12)
 
-    label_str = '$A(V)_1 = {:3.1f}$, $A(V)_2 = {:3.1f}$, $Att(V) = {:3.2f}$'
-    ax.plot(1./x, att12/att12[sindxs[0]], 
-            label=label_str.format(av1[k],av2[k],att12[sindxs[0]]))
+    label_str = "$A(V)_1 = {:3.1f}$, $A(V)_2 = {:3.1f}$, $Att(V) = {:3.2f}$"
+    ax.plot(
+        1.0 / x,
+        att12 / att12[sindxs[0]],
+        label=label_str.format(av1[k], av2[k], att12[sindxs[0]]),
+    )
 
-ax.set_xlabel('$\lambda$ [$\mu m$]')
-ax.set_ylabel('$Att(\lambda)/Att(V)$')
+ax.set_xlabel(r"$\lambda$ [$\mu m$]")
+ax.set_ylabel(r"$Att(\lambda)/Att(V)$")
 
-ax.set_xscale('log')
-ax.set_xlim(1./10.,1.0/0.3)
+ax.set_xscale("log")
+ax.set_xlim(1.0 / 10.0, 1.0 / 0.3)
 
-ax.legend(loc='best', fontsize=14)
+ax.legend(loc="best", fontsize=14)
 
-ax.tick_params(which='major', length=7)
-ax.tick_params(which='minor', length=4)
+ax.tick_params(which="major", length=7)
+ax.tick_params(which="minor", length=4)
 
 plt.tight_layout()
 
-fig.savefig('2star_mix.pdf')
+fig.savefig("2star_mix.pdf")
--- plot_fm90.py
+++ plot_fm90.py
@@ -2,28 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import astropy.units as u
 
-from dust_extinction.dust_extinction import FM90
+from dust_extinction.shapes import FM90
 
 fig, ax = plt.subplots()
 
 # generate the curves and plot them
-x = np.arange(3.8,8.6,0.1)/u.micron
+x = np.arange(3.8, 8.6, 0.1) / u.micron
 
 ext_model = FM90()
-ax.plot(x,ext_model(x),label='total')
+ax.plot(x, ext_model(x), label="total")
 
 ext_model = FM90(C3=0.0, C4=0.0)
-ax.plot(x,ext_model(x),label='linear term')
+ax.plot(x, ext_model(x), label="linear term")
 
 ext_model = FM90(C1=0.0, C2=0.0, C4=0.0)
-ax.plot(x,ext_model(x),label='bump term')
+ax.plot(x, ext_model(x), label="bump term")
 
 ext_model = FM90(C1=0.0, C2=0.0, C3=0.0)
-ax.plot(x,ext_model(x),label='FUV rise term')
+ax.plot(x, ext_model(x), label="FUV rise term")
 
-ax.set_xlabel('$x$ [$\mu m^{-1}$]')
-ax.set_ylabel('$E(\lambda - V)/E(B - V)$')
+ax.set_xlabel("$x$ [$\mu m^{-1}$]")
+ax.set_ylabel("$E(\lambda - V)/E(B - V)$")
 
-ax.legend(loc='best')
+ax.legend(loc="best")
 
-fig.savefig('sfchap_uvext_comps.pdf')
+fig.savefig("sfchap_uvext_comps.pdf")
